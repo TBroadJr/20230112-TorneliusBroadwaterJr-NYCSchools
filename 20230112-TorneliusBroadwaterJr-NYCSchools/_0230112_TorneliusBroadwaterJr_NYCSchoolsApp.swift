d59f5cbe66574a475913b7a49405b30fc205ab34
Tornelius <tkbjr28@icloud.com>
2023-01-12 12:00:16 -0500

    Initial Commit

---
+++ 20230112-TorneliusBroadwaterJr-NYCSchools/_0230112_TorneliusBroadwaterJr_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0230112_TorneliusBroadwaterJr_NYCSchoolsApp.swift
+//  20230112-TorneliusBroadwaterJr-NYCSchools
+//
+//  Created by Tornelius Broadwater, Jr on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct _0230112_TorneliusBroadwaterJr_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
